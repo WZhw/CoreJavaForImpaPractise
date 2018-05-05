871eb81954630df7bb8e0991c0115b29bf50466e
WZhw <jerrywzw@gmail.com>
2018-05-05 21:30:31 +0800

    Add try catch

--- src/ch01/Prac1ReadInteger.java
+++ src/ch01/Prac1ReadInteger.java
@@ -1,30 +1,35 @@
 package ch01;
+
 import java.util.*;
 
 public class Prac1ReadInteger {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		String inputLine = "";
-		while(in.hasNextLine()) {
-			inputLine = in.nextLine();
-			int originInteger = Integer.parseInt(inputLine);
-			//int binaryInteger = Integer.parseInt(inputLine, 2);
-			//int octalInteger = Integer.parseInt(inputLine, 8);
-			//int hexInteger = Integer.parseInt(inputLine, 16);
-			float Reciprocal = (float) 1.0 / originInteger;
-			float hexfloatReciprocal = Float.floatToIntBits(Reciprocal);
-			System.out.print(Integer.toBinaryString(originInteger));
-			System.out.print(" ");
-			System.out.print(Integer.toOctalString(originInteger));
-			System.out.print(" ");
-			System.out.print(Integer.toHexString(originInteger));
-			System.out.print(" ");
-			System.out.print(Float.toString(Reciprocal));
-			System.out.print(" ");
-			System.out.print(Float.toHexString(hexfloatReciprocal));
-			System.out.print(" ");
-			System.out.println();
+		try {
+			while (in.hasNextLine()) {
+				inputLine = in.nextLine();
+				int originInteger = Integer.parseInt(inputLine);
+				// int binaryInteger = Integer.parseInt(inputLine, 2);
+				// int octalInteger = Integer.parseInt(inputLine, 8);
+				// int hexInteger = Integer.parseInt(inputLine, 16);
+				float Reciprocal = (float) 1.0 / originInteger;
+				float hexfloatReciprocal = Float.floatToIntBits(Reciprocal);
+				System.out.print(Integer.toBinaryString(originInteger));
+				System.out.print(" ");
+				System.out.print(Integer.toOctalString(originInteger));
+				System.out.print(" ");
+				System.out.print(Integer.toHexString(originInteger));
+				System.out.print(" ");
+				System.out.print(Float.toString(Reciprocal));
+				System.out.print(" ");
+				System.out.print(Float.toHexString(hexfloatReciprocal));
+				System.out.print(" ");
+				System.out.println();
+			}
+		} catch (Exception e) {
+		} finally {
+			in.close();
 		}
-		in.close();
 	}
 }
